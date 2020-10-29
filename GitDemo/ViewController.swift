095dd6e71ad8183a65cd817d12f37e18e1d76260
Egeniy Tarasov <tarasov@ucontent.ru>
2020-10-29 13:27:00 +0300

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Евгений Тарасов on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
